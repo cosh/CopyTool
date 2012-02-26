6a6cd726032c0da055e2f07f88dd61c60b248298
Henning Rauch <home@dreiundzwanzig.biz>
2012-02-26 12:15:22 +0100

    at least one bugfix :)

--- KopierJochen/Program.cs
+++ KopierJochen/Program.cs
@@ -18,7 +18,7 @@ namespace KopierJochen
             foreach (var aFile in referenceFiles.Where(_ => !notCompleteFiles.ContainsKey(_.Key)))
             {
                 Console.WriteLine(aFile.Key);
-                File.Copy(aFile.Value, notCompleteDirectory + alternateDirectory + aFile.Key);
+                File.Copy(aFile.Value, alternateDirectory + aFile.Key);
             }
             Console.WriteLine("done");
             Console.ReadLine();
