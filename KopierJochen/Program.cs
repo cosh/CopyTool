fd0c7ae369d2676b0c09ce2095d8db8b4408d6b0
Henning Rauch <home@dreiundzwanzig.biz>
2012-02-26 12:08:25 +0100

    finished kopierjochen

---
+++ KopierJochen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace KopierJochen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string referenceDirectory = @"***";
+            const string notCompleteDirectory = @"***";
+            const string alternateDirectory = @"***";
+
+            var referenceFiles = Directory.EnumerateFiles(referenceDirectory, "*.*").ToDictionary(key => key.Split('\\').Last(), value => value);
+            var notCompleteFiles = Directory.EnumerateFiles(notCompleteDirectory, "*.*").ToDictionary(key => key.Split('\\').Last(), value => value);
+
+            foreach (var aFile in referenceFiles.Where(_ => !notCompleteFiles.ContainsKey(_.Key)))
+            {
+                Console.WriteLine(aFile.Key);
+                File.Copy(aFile.Value, notCompleteDirectory + alternateDirectory + aFile.Key);
+            }
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+    }
+}
